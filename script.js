fe4da9bbf0dd45aea4d8f118f8cb9c423126d197
Siddhant Anand Jadhav <77883564+SiddhantJ007@users.noreply.github.com>
2025-05-21 23:13:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -230,7 +230,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       }
       $('#feedbackTable').DataTable({ pageLength:5, order:[[0,'desc']] });
             }
-          } catch { console.warn('Backend offline – table not refreshed'); }
+          catch { console.warn('Backend offline – table not refreshed'); }
         }
         
         /* manual refresh */
